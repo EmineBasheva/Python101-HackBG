b18c28ef61c94d521e5e224e965dc13a8375fc0c
EmineBasheva <eminka23@abv.bg>
2015-06-02 13:25:32 +0300

    4 spaces fixed

--- week2/problems-with-files/secondProblem/cat2.py
+++ week2/problems-with-files/secondProblem/cat2.py
@@ -2,19 +2,19 @@ import sys
 
 
 def has_arguments():
-	return len(sys.argv[1:]) > 0
+    return len(sys.argv[1:]) > 0
 
 def main():
-	arguments = sys.argv[1:]
-	if has_arguments():
-		for argument in arguments:
-			the_file = open(argument, "r")
-			content = the_file.read()
-			print(content)
-			the_file.close()
-	else:
-		print("There are no arguments :(")
+    arguments = sys.argv[1:]
+    if has_arguments():
+        for argument in arguments:
+            the_file = open(argument, "r")
+            content = the_file.read()
+            print(content)
+            the_file.close()
+    else:
+        print("There are no arguments :(")
 
 if __name__ == '__main__':
-	main()
-	
+    main()
+    
