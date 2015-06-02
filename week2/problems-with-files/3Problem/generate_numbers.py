94dcbeb6dfe11fd979dcef36a793e096178a8f5a
EmineBasheva <eminka23@abv.bg>
2015-06-02 13:33:01 +0300

    remove comments

--- week2/problems-with-files/3Problem/generate_numbers.py
+++ week2/problems-with-files/3Problem/generate_numbers.py
@@ -1,24 +1,20 @@
 import sys
 from random import randint
-# print(randint(1, 1000))
-
 
 def random_n_integers(n):
-	return [str(randint(1, 1000)) for x in range(0, n)]
-
-# print(type("\n".join(random_n_integers(5))) is str)
+    return [str(randint(1, 1000)) for x in range(0, n)]
 
 
 def main():
-	arguments = sys.argv[1:]
-	if len(arguments) == 2:
-		# print(len(arguments))
-		the_file = open(arguments[0], 'w')
-		conntents = random_n_integers(int(arguments[1]))
-		the_file.write(" ".join(conntents) + "\n")
-		the_file.close()
-	else:
-		print("Incorect arguments")
+    arguments = sys.argv[1:]
+    if len(arguments) == 2:
+        # print(len(arguments))
+        the_file = open(arguments[0], 'w')
+        conntents = random_n_integers(int(arguments[1]))
+        the_file.write(" ".join(conntents) + "\n")
+        the_file.close()
+    else:
+        print("Incorect arguments")
 
 if __name__ == '__main__':
-	main()
+    main()
