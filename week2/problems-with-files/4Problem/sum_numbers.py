807dbd92ecadec32c1d75121c4f3a6304a38d185
EmineBasheva <eminka23@abv.bg>
2015-06-02 13:36:31 +0300

    4 spaces fixed

--- week2/problems-with-files/4Problem/sum_numbers.py
+++ week2/problems-with-files/4Problem/sum_numbers.py
@@ -2,15 +2,15 @@ import sys
 
 
 def main():
-	argument = sys.argv[1:]
+    argument = sys.argv[1:]
 
-	the_file = open(argument[0], 'r')
-	contents = the_file.read()
-	the_file.close()
-	sum_integers = sum([int(num) for num in contents.split(" ")])
+    the_file = open(argument[0], 'r')
+    contents = the_file.read()
+    the_file.close()
+    sum_integers = sum([int(num) for num in contents.split(" ")])
 
-	print(sum_integers)
+    print(sum_integers)
 
 
 if __name__ == '__main__':
-	main()
+    main()
